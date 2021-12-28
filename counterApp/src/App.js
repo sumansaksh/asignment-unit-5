b0c5c12f8cc35443ab3736ee25e1939d8a0ff036
sumansaksh <suman_fw12_142@masai.school>
2021-12-29 00:15:27 +0530

    counterApp

---
+++ counterApp/src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+// import React, { useEffect, useState } from "react";
+import { useDebugValue, useState } from "react"
+export default function App() {
+  //const [count, setCount] = useState(10);
+  //let count=0;
+  const [count, setCount] = useState(0)
+  
+  const addOne = (value)=>{
+    setCount(count+value)
+  }
+
+  const double = (value)=>{
+    setCount(count*value)
+  }
+  return (
+    <div className="App">
+      <h3>counter:{count}</h3>
+      <button onClick={()=>addOne(1)}>increment</button>
+      <button onClick={()=>addOne(-1)}>decrement</button>
+      <button onClick={()=>double(2)}>double</button>
+    </div>
+  );
+}
